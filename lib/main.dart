aa9d204316af40be2558b43382181d7566babd73
Webster Dias Wolak <84366225+WebsterDias@users.noreply.github.com>
2022-06-01 13:42:03 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:miniprojeto0/views/home.dart';
+import 'package:miniprojeto02/views/home.dart';
 
 void main() {
   runApp(const MaterialApp(
